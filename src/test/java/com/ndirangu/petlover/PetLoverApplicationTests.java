46800d0c3f56a64ed3e0919a159e8f18ff2a8660
ndirangu <ndirangu.muchai@gmail.com>
2020-10-18 21:04:24 +0300

    Project setup

---
+++ src/test/java/com/ndirangu/petlover/PetLoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndirangu.petlover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
